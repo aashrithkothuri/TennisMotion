31d00ab54b2bf1b61da7de183b6fecf7c13e7029
Prasanth Kothuri <prasanth.kothuri@cern.ch>
2025-08-19 17:01:57 +0100

    Initial Commit

---
+++ TennisMotion Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TennisMotion Watch App
+//
+//  Created by Prasanth Kothuri on 19.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TennisMotion Watch App/TennisMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisMotionApp.swift
+//  TennisMotion Watch App
+//
+//  Created by Prasanth Kothuri on 19.08.25.
+//
+
+import SwiftUI
+
+@main
+struct TennisMotion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
