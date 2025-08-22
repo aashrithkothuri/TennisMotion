3589f4cd5ffea03282529f7676b6cb6aaa6cf9f0
aashrithkothuri <aashrith.kothuri@gmail.com>
2025-08-22 13:58:07 +0100

    Added script to detect gyro and accelerometer measurements and display them on watch, slight change to iOS app

--- TennisMotion Watch App/ContentView.swift
+++ TennisMotion Watch App/ContentView.swift
@@ -5,8 +5,10 @@ import CoreMotion
 let MotionManager = CMMotionManager()
 
 struct ContentView: View {
-    // State variable to store the accelerometer data
+    // State variables to store sensor data
     @State private var acc: [Float64] = [0.0, 0.0, 0.0]
+    @State private var gyro: [Float64] = [0.0, 0.0, 0.0]
+    
     
     // Body
     var body: some View {
@@ -16,18 +18,24 @@ struct ContentView: View {
             // Display accelerometer data
             Text("X: \(acc[0]), Y: \(acc[1]), Z: \(acc[2])")
                 .padding()
+            Text("Gyro data:")
+            
+            // Display accelerometer data
+            Text("X: \(gyro[0]), Y: \(gyro[1]), Z: \(gyro[2])")
+                .padding()
         }
         .onAppear {
             // Start accelerometer data collection when the view appears
-            startAccelerometerUpdates()
+            startAccUpdates()
+            startGyroUpdates()
         }
     }
     
     // Function to get accelerometer data and update the state variable
-    func startAccelerometerUpdates() {
+    func startAccUpdates() {
         // Check if the accelerometer is available
         if MotionManager.isAccelerometerAvailable {
-            // Set the update interval (0.1 Hz)
+            // Set the update interval (0.1s,10 Hz)
             MotionManager.accelerometerUpdateInterval = 0.1
 
             // Start receiving accelerometer updates asynchronously
@@ -53,6 +61,41 @@ struct ContentView: View {
         } else {
             print("Accelerometer data not available")
         }
+    }
+    
+    // Function to start gyroscope updates
+    func startGyroUpdates() {
+        
+        // Check if available
+        if MotionManager.isGyroAvailable {
+            
+            // Set update interval (0.1s,10Hz)
+            MotionManager.gyroUpdateInterval = 0.1
+            
+            // Start getting data (async)
+            MotionManager.startGyroUpdates(to:OperationQueue.current!) { (data, error) in
+                
+                // If error exists
+                if let error = error {
+                    print("Error getting gyro data \(error.localizedDescription)")
+                }
+                
+                // If there is data, update data
+                if let data = data {
+                    let x = data.rotationRate.x
+                    let y = data.rotationRate.y
+                    let z = data.rotationRate.z
+
+                    // Update the state variable with the new data
+                    DispatchQueue.main.async {
+                        self.gyro = [x, y, z]
+                    }
+                }
+            }
+        } else {
+            print("Gyro data not available")
+        }
+        
     }
 }
 
